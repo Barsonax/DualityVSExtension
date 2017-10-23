875f1ed1f4f5a22a2dbe8be040b4dca51c4e0011
Barsonax <rick-van-dam@outlook.com>
2017-10-23 16:28:08 +0200

    master: first commit

---
+++ DualityComponent/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DualityComponent")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rick van Dam")]
+[assembly: AssemblyProduct("DualityComponent")]
+[assembly: AssemblyCopyright("Copyright © Rick van Dam 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76940b73-9e48-449a-b20a-9e6645e9e12f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DualityEditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace $safeprojectname$
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+	public class $safeprojectname$ : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "$safeprojectname$"; }
+		}
+	}
+}
